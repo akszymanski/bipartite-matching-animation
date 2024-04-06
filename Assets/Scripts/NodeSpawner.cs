425684fe1e27a9c8fe9b2c0c45ea0da7312a261d
Ally <akszymanski@crimson.ua.edu>
2024-04-06 18:10:00 -0500

    animate Steps

--- Assets/Scripts/NodeSpawner.cs
+++ Assets/Scripts/NodeSpawner.cs
@@ -9,7 +9,7 @@ using System.Linq;
 
 public class NodeSpawner : MonoBehaviour
 {
-
+    public float time = 0;
     public int leftNodeCount = 0;
     public int rightNodeCount = 0;
     public GameObject leftNodeParent;
@@ -65,10 +65,10 @@ public class NodeSpawner : MonoBehaviour
         // Remove "Initialize: " from the start of the string, then remove the parentheses
         string sets = step.Replace("Initialize: ", "").Replace("(", "").Replace(")", "");
 
-        // Split the string into two parts on the space character
+        // Split the string into two parts on the space
         string[] splitSets = sets.Split(' ');
 
-        // Split each part on the "," character to get the individual numbers and letters
+        // Split each part on the  comma
         string[] firstSet = splitSets[0].Split(',');
         string[] secondSet = splitSets[1].Split(',');
 
@@ -77,6 +77,33 @@ public class NodeSpawner : MonoBehaviour
 
         return (firstSet, secondSet);
     }
+
+    void AddEdges(List<(string, string)> edges) {
+        // Create a new Material
+        Material material = new Material(Shader.Find("Unlit/Color"));
+        material.color = Color.white; // Set the color to white
+
+        foreach (var edge in edges) {
+
+            Debug.Log("Adding edgees" + edge.Item1 + " " + edge.Item2);
+            // Find nodes in pair
+            var node1 = GameObject.Find("Node" + edge.Item1);
+            var node2 = GameObject.Find("Node" + edge.Item2);
+
+            var edgeObject = new GameObject("Edge" + edge.Item1 + "_" + edge.Item2);
+            var lineRenderer = edgeObject.AddComponent<LineRenderer>();
+
+            lineRenderer.material = material;
+
+            // Get the edge positions
+            lineRenderer.SetPosition(0, node1.transform.position);
+            lineRenderer.SetPosition(1, node2.transform.position);
+
+            lineRenderer.startWidth = 0.05f;
+            lineRenderer.endWidth = 0.05f;
+        }
+    }
+
 
     void animateStep(string step) {
         //parse the step and animate it
@@ -94,9 +121,80 @@ public class NodeSpawner : MonoBehaviour
             Debug.Log("Second Set: " + string.Join(",", secondSet));
             SpawnNodes(firstSet.Length, leftNodeParent, leftColor, firstSet);
             SpawnNodes(secondSet.Length, rightNodeParent, rightColor, secondSet);
+        } 
+        else if (step.StartsWith("Add_edge:")) {
+            Debug.Log("Adding edges");
+            List<(string, string)> edges = ParseStep(step);
+            AddEdges(edges);
+        }
+        else if (step.StartsWith("Add_path:")) {
+            List<(string, string)> edges = ParseStep(step);
+            time = time + 1;
+            for (int i = 0; i < edges.Count; i++) {
+                var edge = edges[i];
+                Color color = i % 2 == 0 ? Color.green : Color.black;
+                StartCoroutine(ChangeEdgeColorWaiter(time, "Edge" + edge.Item1 + "_" + edge.Item2, Color.green));
+            }
+        } else if (step.StartsWith("Update_match:")){
+            List<(string, string)> edges = ParseStep(step);
+            time = time + 1;
+            foreach (var edge in edges) {
+                StartCoroutine(ChangeEdgeColorWaiter(time, "Edge" + edge.Item1 + "_" + edge.Item2, Color.yellow));
+            }
+        } else if (step.StartsWith("Disregard_vertices:")){
+            List<(string, string)> edges = ParseStep(step);
+            time = time + 1;
+            foreach (var edge in edges) {
+                StartCoroutine(ChangeEdgeColorWaiter(time, "Edge" + edge.Item1 + "_" + edge.Item2, Color.grey));
+            }
         }
 
         //TODO: add more steps
+    }
+
+    void ChangeEdgeColor(string edgeName, Color newColor) {
+        // find the edge
+        var edgeObject = GameObject.Find(edgeName);
+
+        if (edgeObject == null) {
+            Debug.LogError("Edge not found: " + edgeName);
+            return;
+        }
+
+        var lineRenderer = edgeObject.GetComponent<LineRenderer>();
+        
+
+        // Change the color of the edge
+        Material material = new Material(Shader.Find("Unlit/Color"));
+        material.color = newColor;
+        lineRenderer.material = material;
+    }
+
+    IEnumerator ChangeEdgeColorWaiter(float waitTime, string edgeName, Color newColor) {
+        // wait for the time
+        yield return new WaitForSeconds(waitTime);
+        ChangeEdgeColor(edgeName, newColor);
+    }
+
+    List<(string, string)> ParseStep(string step) {
+        // Remove step before the semicolon
+        int colonIndex = step.IndexOf(':');
+        string pairs = step.Substring(colonIndex + 1).Trim();
+        pairs = pairs.Replace("(", "").Replace(")", "");
+
+        // Split the string into pairs
+        string[] splitPairs = pairs.Split(' ');
+
+        // Create a list to hold the parsed pairs
+        List<(string, string)> parsedPairs = new List<(string, string)>();
+
+        // Parse each pair
+        foreach (string pair in splitPairs) {
+            string[] nodes = pair.Split(',');
+            parsedPairs.Add((nodes[0], nodes[1]));
+        }
+
+        return parsedPairs;
     }
 
     // Start is called before the first frame update
@@ -121,4 +219,5 @@ public class NodeSpawner : MonoBehaviour
     {
         
     }
-}
+    }
+
